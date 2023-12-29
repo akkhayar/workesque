aa1887e3a53ab63aea50cf46cf175c0b467929e7
Rickaym <psmtitant@gmail.com>
2023-12-29 18:59:10 +0630

    format: apply prettier

--- src/pages/api/check-drive-access.ts
+++ src/pages/api/check-drive-access.ts
@@ -1,11 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import getGoogleClient from '@/utils/gdrive';
+import type { NextApiRequest, NextApiResponse } from "next";
+import getGoogleClient from "@/utils/gdrive";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     try {
       const drive = await getGoogleClient();
 
@@ -14,16 +14,16 @@ export default async function handler(
 
       const response = await drive.files.get({
         fileId: fileId,
-        fields: 'id, name'
+        fields: "id, name",
       });
 
       res.status(200).json({ fileName: response.data.name });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Error accessing Google Drive' });
+      res.status(500).json({ error: "Error accessing Google Drive" });
     }
   } else {
-    res.setHeader('Allow', ['POST']);
+    res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
--- src/utils/gdrive.ts
+++ src/utils/gdrive.ts
@@ -1,27 +1,27 @@
 // utils/googleDriveClient.ts
-import { google } from 'googleapis';
-import { readFileSync } from 'fs';
-import type { JWT } from 'google-auth-library';
-import path from 'path';
+import { google } from "googleapis";
+import { readFileSync } from "fs";
+import type { JWT } from "google-auth-library";
+import path from "path";
 
 let jwtClient: JWT | null = null;
 
 export async function getGoogleClient() {
   if (!jwtClient) {
-    const keyPath = path.join(process.cwd(), 'gdrive-key.json');
-    const key = JSON.parse(readFileSync(keyPath, 'utf-8'));
+    const keyPath = path.join(process.cwd(), "gdrive-key.json");
+    const key = JSON.parse(readFileSync(keyPath, "utf-8"));
 
     jwtClient = new google.auth.JWT(
       key.client_email,
       undefined,
       key.private_key,
-      ['https://www.googleapis.com/auth/drive']
+      ["https://www.googleapis.com/auth/drive"]
     );
 
     await jwtClient.authorize();
   }
 
-  return google.drive({ version: 'v3', auth: jwtClient });
+  return google.drive({ version: "v3", auth: jwtClient });
 }
 
 export default getGoogleClient;
