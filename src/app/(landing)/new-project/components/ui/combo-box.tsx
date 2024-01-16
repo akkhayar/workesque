048d6bc9af0e900790ba5941cad608e08b7d804f
SaiSeng18 <saisengmongli14@gmail.com>
2024-01-16 23:54:32 +0630

    feat: install shadcn components

---
+++ src/app/(landing)/new-project/components/ui/combo-box.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+} from "@/components/ui/command";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+
+const frameworks = [
+	{
+		value: "next.js",
+		label: "Next.js",
+	},
+	{
+		value: "sveltekit",
+		label: "SvelteKit",
+	},
+	{
+		value: "nuxt.js",
+		label: "Nuxt.js",
+	},
+	{
+		value: "remix",
+		label: "Remix",
+	},
+	{
+		value: "astro",
+		label: "Astro",
+	},
+];
+
+const ComboBox = ({
+	onValueChange,
+}: {
+	onValueChange: (value: string) => void;
+}) => {
+	const [open, setOpen] = React.useState(false);
+	const [value, setValue] = React.useState("");
+
+	return (
+		<Popover open={open} onOpenChange={setOpen}>
+			<PopoverTrigger asChild>
+				<Button
+					variant="outline"
+					role="combobox"
+					aria-expanded={open}
+					className="justify-between w-[250px] h-[60px] focus:outline-none focus:ring-0 text-muted-foreground">
+					{value
+						? frameworks.find((framework) => framework.value === value)?.label
+						: "Select framework..."}
+					<ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-[170px] p-[15px]">
+				<Command>
+					<CommandInput />
+					<CommandEmpty>No framework found.</CommandEmpty>
+					<CommandGroup>
+						{frameworks.map((framework) => (
+							<CommandItem
+								key={framework.value}
+								value={framework.value}
+								onSelect={(currentValue: string) => {
+									onValueChange(currentValue);
+									setValue(currentValue === value ? "" : currentValue);
+									setOpen(false);
+								}}>
+								<Check
+									className={cn(
+										"mr-2 h-4 w-4",
+										value === framework.value ? "opacity-100" : "opacity-0"
+									)}
+								/>
+								{framework.label}
+							</CommandItem>
+						))}
+					</CommandGroup>
+				</Command>
+			</PopoverContent>
+		</Popover>
+	);
+};
+export default ComboBox;
